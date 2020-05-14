8644d65cd732c17e492276ffee12339ece01fdd3
jorgemartiinez19 <jorgemartiinez19@gmail.com>
2020-05-14 20:18:31 +0200

    seccion sobre mi avanzada

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 var menuIcon = $('#menu-icon');
 var crossIcon = $('#cross-icon');
 var nav = $('.nav');
-var myFullpage = new fullpage('#fullpage');
+// var myFullpage = new fullpage('#fullpage');
 
 $(document).ready(function () {
   menuIcon.click(openMenu);
   crossIcon.click(closeMenu);
-});
 
+});
+// $(document).alton({
+//   fullSlideContainer: 'full', // Tell Alton the full height container
+//   singleSlideClass: 'slide', // Tell Alton the full height slide class
+//   useSlideNumbers: true, // Set to false if you don't want to use pagination
+//   slideNumbersBorderColor: '#fff', // Set the outside color of the pagination items
+//   slideNumbersColor: 'transparent', // Set the inner color of the pagination items
+//   bodyContainer: 'body', // Tell Alton the body class
+// });
 function openMenu() {
   nav.addClass('show-flex');
   nav.animate(
     {
-      height: '100vh',
       width: '100%',
-      opacity: '1'
+      opacity: '1',
     },
     {
       duration: 400,
@@ -29,8 +36,7 @@ function closeMenu() {
   nav.animate(
     {
       width: '0',
-      height: '0',
-      opacity: '0'
+      opacity: '0',
     },
     {
       duration: 350,
