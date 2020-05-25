d81ea543243040ec9143106b9c4c834a4e4fe236
jorgemartiinez19 <jorgemartiinez19@gmail.com>
2020-05-25 21:28:21 +0200

    añadida página error 404 y nuevo trabajo

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="css/main.min.css" />
+    <title>Página no encontrada</title>
+</head>
+
+<body>
+    <div style="display: flex; align-items:center; justify-content: center; flex-direction: column; padding-top: 5rem; height: 100%; width: 100% ">
+        <h2 class="heading-secondary u-margin-bottom-small u-text-center" style="color: #000">Error</h2>
+        <h2 class="heading-secondary u-margin-bottom-medium u-text-center" style="color: #000">Página no encontrada</h2>
+        <div class="u-text-center">
+            <a href="https://jorge-martinez.es/" class="btn btn--portfolio u-text-center" style="text-decoration: none">Ir a la página principal</a>
+        </div>
+    </div>
+</body>
+
+</html>
